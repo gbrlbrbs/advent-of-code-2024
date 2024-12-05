af008a59c698fedf0bd1d28ee0b705ed487a4948
gbrlbrbs <gabriel.barbosa.martinz@outlook.com>
2024-12-05 12:18:04 -0300

    day4 end

--- lib/advent_of_code/day4.ex
+++ lib/advent_of_code/day4.ex
@@ -1,18 +1,21 @@
 defmodule AdventOfCode.Day4 do
-  @directions [{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}]
+  @x_directions [{1, 1}, {1, -1}, {-1, 1}, {-1, -1}]
+  @directions [{0, 1}, {0, -1}, {1, 0}, {-1, 0}] ++ @x_directions
 
   defp valid_position?({y, x}, {m, n}) do
     x >= 0 and x < n and y >= 0 and y < m
   end
 
-  @spec search(list(binary()), binary()) :: list()
-  def search(lines, word) do
+  @spec search(list(binary()), binary(), boolean()) :: list()
+  def search(lines, word, use_x \\ false) do
     m = length(lines)
     n = String.length(hd(lines))
 
+    dirs = if use_x, do: @x_directions, else: @directions
+
     for row <- 0..m-1,
         col <- 0..n-1,
-        dir <- @directions,
+        dir <- dirs,
         check_word(lines, word, {row, col}, dir, {m, n}) do
           {{row, col}, dir}
     end
@@ -36,5 +39,12 @@ defmodule AdventOfCode.Day4 do
   @spec get_letter(list(binary()), integer(), integer()) :: nil | binary()
   defp get_letter(grid, row, col) do
     grid |> Enum.at(row) |> String.at(col)
+  end
+
+  @spec calculate_center(list(tuple()), integer()) :: list()
+  def calculate_center(found_positions, center_idx) do
+    found_positions |> Enum.map(fn {{row, col}, {dy, dx}} ->
+      {row + center_idx * dy, col + center_idx * dx}
+    end)
   end
 end
--- scripts/day4.exs
+++ scripts/day4.exs
@@ -8,3 +8,13 @@ found = lines |> AdventOfCode.Day4.search("XMAS")
 num_found = length(found)
 
 IO.puts("Number of found words: #{num_found}")
+
+found_x_mas = lines |> AdventOfCode.Day4.search("MAS", true)
+mas_centers = found_x_mas |> AdventOfCode.Day4.calculate_center(1)
+  |> Enum.frequencies()
+
+IO.inspect(mas_centers)
+
+num_found_x_mas = mas_centers |> Enum.count(fn {_, v} -> v == 2 end)
+
+IO.puts("Found X-MAS: #{num_found_x_mas}")
