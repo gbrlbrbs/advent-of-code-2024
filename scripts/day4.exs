1acbd63397ed77e33ce50334f17f945828199613
gbrlbrbs <gabriel.barbosa.martinz@outlook.com>
2024-12-06 16:25:16 -0300

    add alias in mix

--- scripts/day4.exs
+++ scripts/day4.exs
@@ -13,8 +13,6 @@ found_x_mas = lines |> AdventOfCode.Day4.search("MAS", true)
 mas_centers = found_x_mas |> AdventOfCode.Day4.calculate_center(1)
   |> Enum.frequencies()
 
-IO.inspect(mas_centers)
-
 num_found_x_mas = mas_centers |> Enum.count(fn {_, v} -> v == 2 end)
 
 IO.puts("Found X-MAS: #{num_found_x_mas}")
