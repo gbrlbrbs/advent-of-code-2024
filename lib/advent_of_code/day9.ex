af367b1349a78154c34206316227afc4a01b5a26
gbrlbrbs <gabriel.barbosa.martinz@outlook.com>
2024-12-12 11:21:04 -0300

    format

--- lib/advent_of_code/day9.ex
+++ lib/advent_of_code/day9.ex
@@ -11,8 +11,9 @@ defmodule AdventOfCode.Day9 do
 
     def new(), do: %DiskMapEntry{}
     def new_free(offset, size), do: %DiskMapEntry{offset: offset, size: size}
-    def new_file(id, offset, size), do:
-          %DiskMapEntry{type: :file, offset: offset, size: size, id: id}
+
+    def new_file(id, offset, size),
+      do: %DiskMapEntry{type: :file, offset: offset, size: size, id: id}
   end
 
   @spec parse_input(binary()) :: {list(), list()}
@@ -27,13 +28,13 @@ defmodule AdventOfCode.Day9 do
   def checksum_dense(line) do
     {files, free} = parse_input(line)
     compacted = dense_compact(files |> Enum.reverse(), free)
-    {checksum(compacted), compacted |> Enum.sort_by(&(&1.offset))}
+    {checksum(compacted), compacted |> Enum.sort_by(& &1.offset)}
   end
 
   def checksum_move(line) do
     {files, free} = parse_input(line)
     moved_files = move_files(files |> Enum.reverse(), free)
-    {checksum(moved_files), moved_files |> Enum.sort_by(&(&1.offset))}
+    {checksum(moved_files), moved_files |> Enum.sort_by(& &1.offset)}
   end
 
   defp checksum(files) do
@@ -44,41 +45,51 @@ defmodule AdventOfCode.Day9 do
   end
 
   defp create_blocks(
-    c,
-    {
-      entrynum,
-      %DiskMapEntry{type: :free, offset: off, size: sz}
-    }
-  ), do: {entrynum, %DiskMapEntry{offset: off + sz, size: c - ?0, type: :file, id: entrynum}}
+         c,
+         {
+           entrynum,
+           %DiskMapEntry{type: :free, offset: off, size: sz}
+         }
+       ),
+       do: {entrynum, %DiskMapEntry{offset: off + sz, size: c - ?0, type: :file, id: entrynum}}
+
   defp create_blocks(
-    c,
-    {
-      entrynum,
-      %DiskMapEntry{type: :file, offset: off, size: sz}
-    }
-  ), do: {entrynum + 1, %DiskMapEntry{type: :free, offset: off + sz, size: c - ?0}}
+         c,
+         {
+           entrynum,
+           %DiskMapEntry{type: :file, offset: off, size: sz}
+         }
+       ),
+       do: {entrynum + 1, %DiskMapEntry{type: :free, offset: off + sz, size: c - ?0}}
 
   defp dense_compact([], _), do: []
   defp dense_compact(files, []), do: files
   defp dense_compact(files, [%DiskMapEntry{size: 0} | free]), do: dense_compact(files, free)
-  defp dense_compact(files = [%DiskMapEntry{offset: off_file} | _], [%DiskMapEntry{offset: off_free} | _])
-  when off_file <= off_free, do: files
+
+  defp dense_compact(files = [%DiskMapEntry{offset: off_file} | _], [
+         %DiskMapEntry{offset: off_free} | _
+       ])
+       when off_file <= off_free,
+       do: files
+
   # when whole file can be moved -> edit the free space
   defp dense_compact(
-    [%DiskMapEntry{id: id, size: sz_file} | files],
-    [%DiskMapEntry{offset: off_free, size: sz_free} | free]
-  ) when sz_free >= sz_file do
+         [%DiskMapEntry{id: id, size: sz_file} | files],
+         [%DiskMapEntry{offset: off_free, size: sz_free} | free]
+       )
+       when sz_free >= sz_file do
     [
       DiskMapEntry.new_file(id, off_free, sz_file)
       | dense_compact(files, [DiskMapEntry.new_free(off_free + sz_file, sz_free - sz_file) | free])
     ]
   end
+
   # when some of the file can be moved -> edit the file at the end (beginning of list), create new at the beginning (head of return)
   # free space at head is deleted
   defp dense_compact(
-    [%DiskMapEntry{id: id, size: sz_file, offset: off_file} | files],
-    [%DiskMapEntry{offset: off_free, size: sz_free} | free]
-  ) do
+         [%DiskMapEntry{id: id, size: sz_file, offset: off_file} | files],
+         [%DiskMapEntry{offset: off_free, size: sz_free} | free]
+       ) do
     [
       DiskMapEntry.new_file(id, off_free, sz_free)
       | dense_compact([DiskMapEntry.new_file(id, off_file, sz_file - sz_free) | files], free)
@@ -86,11 +97,14 @@ defmodule AdventOfCode.Day9 do
   end
 
   defp move_files([], _), do: []
+
   defp move_files([file | remaining_files], frees) do
     frees_before = frees |> Enum.filter(&is_before_file?(file, &1))
+
     case find_free_space(file, frees_before) do
       {:not_found, _, _} ->
         [file | move_files(remaining_files, frees_before)]
+
       {:found, new_frees, new_file} ->
         [new_file | move_files(remaining_files, new_frees)]
     end
@@ -101,15 +115,22 @@ defmodule AdventOfCode.Day9 do
   # if not found, return the :not_found atom
   defp find_free_space(file, frees) do
     index = frees |> Enum.find_index(&has_enough_space?(file, &1))
+
     case index do
-      nil -> {:not_found, nil, nil}
+      nil ->
+        {:not_found, nil, nil}
+
       index ->
         free_space = frees |> Enum.at(index)
-        edited_free_space = DiskMapEntry.new_free(free_space.offset + file.size, free_space.size - file.size)
+
+        edited_free_space =
+          DiskMapEntry.new_free(free_space.offset + file.size, free_space.size - file.size)
+
         new_frees =
           frees
           |> List.replace_at(index, edited_free_space)
           |> Enum.filter(&is_before_file?(file, &1))
+
         # need to remove all after the file offset
         new_file = DiskMapEntry.new_file(file.id, free_space.offset, file.size)
         {:found, new_frees, new_file}
@@ -118,5 +139,4 @@ defmodule AdventOfCode.Day9 do
 
   defp has_enough_space?(file, free_space), do: free_space.size >= file.size
   defp is_before_file?(file, free), do: file.offset > free.offset
-
 end
